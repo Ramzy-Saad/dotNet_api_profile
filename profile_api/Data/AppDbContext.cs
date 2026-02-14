1e3a2cef3a5289d38ecc4056f68a1350fe318187
Ramzy Saad <www.ramzysaad16@gmail.com>
2026-02-14 13:20:45 +0200

    init_migration

---
+++ profile_api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using profile_api.Models.Domain;
+using System.Reflection.Metadata;
+
+namespace profile_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- profile_api/Program.cs
+++ profile_api/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using profile_api.Data;
+
 namespace profile_api
 {
     public class Program
@@ -13,6 +16,8 @@ namespace profile_api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("profileConnectionString")));
 
             var app = builder.Build();
 
